9f4c5eb1ed8709c21c21030e96e6a1f85bdb1b2d
bartoszczajkowski <bpczajkowski@gmail.com>
2018-07-26 22:26:24 +0200

    #feature: Login UI with connections
    Added Login UI + LaunchScreen UI + connections + public protocols group

---
+++ LawyerExamApp/UI/Login/LoginViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LoginViewController.swift
+//  LawyerExamApp
+//
+//  Created by Bartosz Czajkowski on 26/07/2018.
+//  Copyright © 2018 BartoszCzajkowski. All rights reserved.
+//
+
+import UIKit
+
+
+
+class LoginViewController: UIViewController {
+    
+
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var usernameImageView: UIImageView!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordImageView: UIImageView!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var signInLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
+
---
+++ LawyerExamApp/Utils/PublicProtocols/PublicProtocols.swift
@@ -0,0 +1,20 @@
+//
+//  PublicProtocols.swift
+//  LawyerExamApp
+//
+//  Created by Bartosz Czajkowski on 26/07/2018.
+//  Copyright © 2018 BartoszCzajkowski. All rights reserved.
+//
+
+import Foundation
+
+public protocol LanguageLocalization {
+    
+}
+public protocol ThemeApplicable {
+    
+}
+
+public protocol FontApplicable {
+    
+}
